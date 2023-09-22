2246607e978ffafec05a0b1c1071a6e54268c5c8
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2023-09-22 13:29:34 +0300

    Adds Vertical Timeline

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,7 +8,7 @@ import { useSectionInView } from '@/lib/hooks';
 export default function Projects() {
   const ref = useSectionInView('Projects', 0.5);
   return (
-    <section className="scroll-mt-28" id="projects" ref={ref}>
+    <section className="scroll-mt-28 mb-28 sm:mb-0" id="projects" ref={ref}>
       <SectionHeading>Projects</SectionHeading>
       <div className="">
         {projectsData.map((project, index) => (
