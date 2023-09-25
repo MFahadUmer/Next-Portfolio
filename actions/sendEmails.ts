aa5e27922b7ab1aa759f1ec62dd75511be3d5396
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2023-09-25 11:28:22 +0300

    Completes Email Functionality

--- actions/sendEmails.ts
+++ actions/sendEmails.ts
@@ -1,11 +1,10 @@
 'use server';
 import { validateString } from '@/lib/utils';
 import { Resend } from 'resend';
-
+import { getErrorMessage } from '@/lib/utils';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-
   const senderEmail = formData.get('senderEmail');
   const message = formData.get('message');
 
@@ -15,11 +14,17 @@ export const sendEmail = async (formData: FormData) => {
   if (!validateString(message, 5000)) {
     return { error: 'Invalid Message' };
   }
-  await resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: 'eng.fahadumer@gmail.com',
-    subject: 'Message from portfolio',
-    reply_to: senderEmail as string,
-    text: message as string,
-  });
+  try {
+    await resend.emails.send({
+      from: 'Contact from <onboarding@resend.dev>',
+      to: 'eng.fahadumer@gmail.com',
+      subject: 'Message from portfolio',
+      reply_to: senderEmail as string,
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
 };
